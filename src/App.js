c46a4edacfe5ca7e08f1278cb2f2d462d1c07aab
Surya Vedantam <suryavedantam@Suryas-Mac-mini.local>
2025-11-29 10:42:10 +0530

    webpack config add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-       Hello World
+       Hello Worlds
       </header>
     </div>
   );
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images'
+                    }
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts'
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        //new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        historyApiFallback: true,
+        
+        port: 9000
+    }
+};
